f847ba9b8d6f8b176dba3fcbe684dc6ecb7d0ecb
Iori205 <b.sodbilegt11@gmail.com>
2025-07-23 17:03:40 +0800

    Homework

--- Object.js
+++ Object.js
@@ -9,6 +9,9 @@
 // let result = findavg(grades);
 // console.log(result);
 
+
+
+
 // function findsum(sum) {
 //   let num = 0;
 //   for (i = 0; i < sum.length; i++) {
@@ -19,28 +22,29 @@
 // let result = findsum(grades);
 // console.log(result);
 
+
+
 let grades = [10, 2, 3, 4, 5, 6, -9, -7, 7, 8, 9, 10, 10];
-// console.log(grades.length);
 
 function even(arr) {
   let num = 0;
   let k = 0;
-
   for (i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
-      //   console.log(arr[i]);
       num = num + arr[i];
     }
-    // // if (arr[i] % 2 == 0) {
-    // //   console.log(arr[i].length);
-    // }
+    if (arr[i] % 2 ===0) {
+      k = k + 1
+    }
   }
-  return num;
+  return num
 }
-let result = even(grades);
-console.log(result);
+let maxgrade = even(grades);
+console.log(maxgrade);
 
-// console.log(grades.length);
+
+
+
 
 // function findmaxgrade(arr) {
 //   let max = arr[0];
@@ -53,3 +57,18 @@ console.log(result);
 // }
 // let maxgrade = findmaxgrade(grades);
 // console.log(maxgrade);
+
+
+// function evenmin(arr) {
+//   let min = arr[0]
+//   for (i=0; i<arr.length; i++) {
+//     if (arr[i] % 2 == 0) {
+//       if (arr[i] < min) {
+//       min = arr[i]
+//     }
+//     }
+//   }
+//   return min
+// }
+// let result = evenmin(grades)
+// console.log(result)
